b3415fbf054150b697c1aefcf4c936f0da61d38d
manaskantha <manas.kantha@gmail.com>
2022-06-06 21:43:56 +0800

    Initial commit

---
+++ src/clainofResp/NegativeProcessor.java
@@ -0,0 +1,19 @@
+package clainofResp;
+
+public class NegativeProcessor extends Processor{
+    public NegativeProcessor(Processor nextProcessor){
+        super(nextProcessor);
+    }
+
+    public void process(Number request)
+    {
+        if (request.getNumber() < 0)
+        {
+            System.out.println("NegativeProcessor : " + request.getNumber());
+        }
+        else
+        {
+            super.process(request);
+        }
+    }
+}
---
+++ src/clainofResp/PositiveProcessor.java
@@ -0,0 +1,19 @@
+package clainofResp;
+
+public class PositiveProcessor extends Processor {
+    public PositiveProcessor(Processor nextProcessor){
+        super(nextProcessor);
+    }
+
+    public void process(Number request)
+    {
+        if (request.getNumber() > 0)
+        {
+            System.out.println("PositiveProcessor : " + request.getNumber());
+        }
+        else
+        {
+            super.process(request);
+        }
+    }
+}
